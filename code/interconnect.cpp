42b490bdd68a95e920b922baf8e7e96b9a1a74b0
liam-gersten <gerstenliam@gmail.com>
2024-05-03 13:12:20 -0400

    Uses MPI_Cancel

--- code/interconnect.cpp
+++ code/interconnect.cpp
@@ -158,12 +158,17 @@ void Interconnect::clean_dead_messages(bool always_free) {
     int flag = 0;
     int message_id;
     request = Interconnect::dead_message_queue.peak_front(&message_id);
+    MPI_Test(&request, &flag, &status);
     if (always_free) {
-      if (PRINT_INTERCONNECT) printf(" I(PID %d explicitly freed message %d)\n", Interconnect::pid, message_id);
+      if (flag) {
+        if (PRINT_INTERCONNECT) printf(" I(PID %d freed message %d)\n", Interconnect::pid, message_id);
+      } else {
+        if (PRINT_INTERCONNECT) printf(" I(PID %d explicitly freed message %d)\n", Interconnect::pid, message_id);
+        MPI_Cancel(&request);
+      }
       message = Interconnect::dead_message_queue.pop_from_front();
       free(message);
     } else {
-      MPI_Test(&request, &flag, &status);
       if (!flag) {
         if (PRINT_INTERCONNECT) printf(" I(PID %d could not free message %d)\n", Interconnect::pid, message_id);
         return;
@@ -197,5 +202,5 @@ void Interconnect::free_interconnect() {
   assert(Interconnect::num_stashed_work == 0);
   free(Interconnect::stashed_work);
   free(Interconnect::work_is_stashed);
-  clean_dead_messages(true);
+  blocking_wait_for_message_delivery();
 }
